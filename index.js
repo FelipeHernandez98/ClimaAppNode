45a33ebabde21e824c2648f8b89623a7e8af2e62
unknown <andresfelipehc@ufps.edu.co>
2022-04-29 14:30:27 -0500

    Se creo la funcionalidad de arrojar el clima, y guardar el historial de las busquedas

--- index.js
+++ index.js
@@ -22,20 +22,29 @@ const main = async () => {
                 const lugares = await busquedas.ciudad(lugar);
                 //Seleccionar un lugar de los lugares del resultado
                 const id = await listarLugares(lugares);
+                if(id === '0')continue;
                 const lugarSel = lugares.find(l => l.id === id );
+                //Gradar en DB
+                busquedas.agregarHistorial(lugarSel.nombre);
                 //Clima
-
+                const clima = await busquedas.clima(lugarSel.lat, lugarSel.lng);
+                //Mostrar información
                 console.log('\nInformación de la ciudad\n'.green);
                 console.log('Ciudad: '+lugarSel.nombre);
                 console.log('Lat: ' + lugarSel.lat);
                 console.log('Lng: ' +lugarSel.lng);
-                console.log('Temperatura: ');
-                console.log('Minima: ');
-                console.log('Maxima: ');
+                console.log('Temperatura: '+clima.temp);
+                console.log('Minima: '+ clima.min);
+                console.log('Maxima: '+ clima.max); 
+                console.log('Descripcion: '+ clima.desc.green);
 
                 break;
 
             case 2:
+                busquedas.historialCapitalizado.forEach((lugar,i) => {
+                    const idx = `${ i + 1}.`.green;
+                    console.log(`${idx} ${lugar}`)
+                })
                 break;
         }
 
--- models/busquedas.js
+++ models/busquedas.js
@@ -1,11 +1,25 @@
+const fs = require('fs')
 const axios = require('axios');
 
 class Busquedas {
 
     historial = [];
+    dbPath = './db/database.json';
 
     constructor() {
+        this.leerDB();
+    }
 
+    //Metodo para poner en mayusculas las iniciales
+    get historialCapitalizado(){
+        return this.historial.map( lugar => { //Se recorre el arreglo
+
+            let palabras = lugar.split(' '); // Se crea el arreglo palabras, y se divide por espacios
+            palabras = palabras.map( p => p[0].toUpperCase() + p.substring(1) ); //Se recorre el arreglo y se pone en mayuscula la primera lettra + el resto
+
+            return palabras.join(' ');//Se uno el arreglo y se envia
+
+        })
     }
 
     get paramsMapbox() {
@@ -16,6 +30,13 @@ class Busquedas {
         }
     }
 
+    get paramsOpenWeather(){
+        return{
+            appid: process.env.OPENWEATHER_KEY,
+            units: 'metric',
+            lang: 'es'
+        }
+    }
     async ciudad(lugar = '') {
 
         try {
@@ -38,6 +59,57 @@ class Busquedas {
             return [];
         }
 
+    }
+
+
+    async clima(lat, lon){
+        try {
+            const instance = axios.create({
+                baseURL: 'https://api.openweathermap.org/data/2.5/weather',
+                params : {...this.paramsOpenWeather, lat, lon}
+            });
+
+            const res = await instance.get();
+            const {weather, main}= res.data;
+            return {
+                desc: weather[0].description,
+                min: main.temp_min,
+                max: main.temp_max,
+                temp: main.temp
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    agregarHistorial(lugar = ''){
+
+        if(this.historial.includes(lugar.toLocaleLowerCase())){
+            return;
+        }
+
+        this.historial = this.historial.splice(0,5);
+
+        this.historial.unshift(lugar.toLocaleLowerCase());
+        this.guardarDB();
+    }
+
+    guardarDB(){
+
+        const payload = {
+            historial: this.historial
+        }
+
+        fs.writeFileSync(this.dbPath, JSON.stringify(payload));
+    }
+
+    leerDB(){
+        if(!fs.existsSync(this.dbPath)) return;
+
+        const info = fs.readFileSync(this.dbPath, {encoding: 'utf-8'});
+        const data = JSON.parse(info);
+
+        this.historial = data.historial;
     }
 }
 
